c2266d0c945a14269afb7ec51f525240fb8a1c7e
Peter Rabinovitch <Peter.M.Rabinovitch@gmail.com>
2021-12-04 13:42:44 -0500

    added code to clean up bad dates, the fifths problem, etc

--- load_data.R
+++ load_data.R
@@ -5,3 +5,32 @@ df <- read_csv("https://raw.githubusercontent.com/prabinov42/MiscData/master/hor
   clean_names() %>%
   filter(finish_time > 100) %>%
   mutate(finish_time = floor(finish_time) + 2 * (finish_time - floor(finish_time)))
+
+dts <- df %>%
+  group_by(date) %>%
+  summarize(
+    min_temp = min(temp),
+    max_temp = max(temp)
+  ) %>%
+  mutate(diff_temp = max_temp - min_temp) %>%
+  filter(diff_temp > 0) %>%
+  pull(date)
+
+df <- df %>%
+  filter(!(date %in% dts)) %>%
+  filter(cond != "Heavy")
+
+dfr <- df %>%
+  group_by(date, racenum) %>%
+  summarize(
+    n=n(),
+    mn = max(hnum)
+  )%>%
+  filter( n == mn) %>%
+  ungroup()
+
+df <- dfr %>%
+  left_join(df, by = c("date"="date", "racenum"="racenum")) %>%
+  select(-n,-mn)
+
+rm(dfr, dts)
